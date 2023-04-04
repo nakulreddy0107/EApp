416a9da5958c23bb9da06c69ea7a3f7a7aa0fa25
nakulreddy0107 <nakulreddy0001@gmail.com>
2023-04-04 14:26:56 +0530

    commit

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,10 +1,12 @@
 using API.Data;
 using API.entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+        [Authorize]
         public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -13,11 +15,14 @@ namespace API.Controllers
         {
             _context = context;
         }
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.MyProperty.ToListAsync();
         }
+
+   
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUsers(int id)
         {
